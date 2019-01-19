cd368639595f3404fad716380e6fb4e0481b76f2
amor20130030328 <1770944692@qq.com>
2019-01-19 20:22:17 +0800

    提交修改

--- java/hello.java
+++ java/hello.java
@@ -3,6 +3,10 @@ public class Hello{
 	public static void main(String [] args){
 	
 		System.out.println("Hello world       Update 001");
+		
+		//fork
+		//git clone https://github.com/15580344451/CRM.git
+		
 	
 	
 	}
